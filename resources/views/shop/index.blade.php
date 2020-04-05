e1f98912371ad9a1d83a4259651483d40212ba05
j.b.sommeling <91102801@mydavinci.nl>
2020-04-05 11:23:20 +0200

    Fout verwijderd uit de bootstrap cards. Nu zijn de auth paginas weer normaal

--- resources/views/shop/index.blade.php
+++ resources/views/shop/index.blade.php
@@ -12,10 +12,10 @@
                 <div class="row">
                     @foreach($productChunk as $product)
                     <div class=" col-12 col-sm-6 col-lg-4 mb-4" align="center">
-                        <div class="card">
-                            <img class="card-img-top" src="{{ $product['imagePath'] }}" alt="Card image cap">
-                            <div class="card-body">
-                                <h5 class="card-title"> {{ $product['title'] }} </h5>
+                        <div class="card product">
+                            <img class="card-img-top product-img-top" src="{{ $product['imagePath'] }}" alt="Card image cap">
+                            <div class="card-body product-body">
+                                <h5 class="card-title product-title"> {{ $product['title'] }} </h5>
                                 <a href="#" class="btn btn-success">In winkelwagen</a>
                                 <a href="#" class="btn btn-secondary">Meer</a>
                             </div>
