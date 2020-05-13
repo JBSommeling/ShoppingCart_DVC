812629f503aa5292962d4122beb6d0321e097d2f
91102801 <91102801@mydavinci.nl>
2020-05-13 10:42:24 +0200

    interface aangepast.

--- app/Cart/iCart.php
+++ app/Cart/iCart.php
@@ -9,5 +9,6 @@ interface iCart
     public function add($product, $product_id, $amount);
     public function editAmount($product, $product_id, $newAmount);
     public function removeProduct($product_id);
+    public function removeCartContent();
     public function addToSession();
 }
